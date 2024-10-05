4980ca3a2ca2d94cb69e011c5cf2669959303527
Andreas-03 <akok0011@student.monash.edu>
2024-10-06 03:50:58 +0800

    Added line plot chart for task 1 and previous map

---
+++ vega_lite_vis.js
@@ -0,0 +1,10 @@
+var vg_1 = "map_schema.json"
+var vg_2 = "line_plot_schema.json"
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#line_plot", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
